23d00190bd93cdb9d4224dbcfd7b98c7f86d7cdb
Felipe Vilella <vilella.luisfelipe@gmail.com>
2019-06-26 21:45:22 -0300

    Repair Feedback message

--- batalha_naval.cpp
+++ batalha_naval.cpp
@@ -5,8 +5,7 @@
 # include <iostream>
 
 // Declara funcao para n gerar erro
-void JogoAlone(std::string nome_do_jogador);
-void JogoTogether(std::string player1, std::string player2);
+void Jogo(std::string nome_do_jogador, std::string nome_do_jogador2);
 
 void LimpaTela(){
     system("clear");
@@ -16,15 +15,14 @@ void LimpaTela(){
 void MenuInicial(){
     int option = 0;
     std::string nome_do_jogador;
-    std::string player1;
-    std::string player2;
+    std::string nome_do_jogador2;
 
     // Laço para opçoes validas
     while (option < 1 || option > 3){
         LimpaTela();
         printf("Welcome to Naval Battle !\n");
-        printf("1 - Play Alone\n");
-        printf("2 - Play Together\n");
+        printf("1 - Play\n");
+        printf("2 - About\n");
         printf("3 - Exit\n");
         printf("Press one option and type ENTER: ");
         scanf("%d", &option);
@@ -32,16 +30,14 @@ void MenuInicial(){
 
     switch (option){
     case 1:
-        printf("What is your name: ");
+        printf("Player1: ");
         std::cin >> nome_do_jogador;
-        JogoAlone(nome_do_jogador);
+        printf("Player2: ");
+        std::cin >> nome_do_jogador2;
+        Jogo(nome_do_jogador, nome_do_jogador2);
         break;
     case 2:
-        printf("Name the Player 1: ");
-        std::cin >> player1;
-        printf("Name the Player 2: ");
-        std::cin >> player2;
-        JogoTogether(player1, player2);
+        printf("About this game !\n");
         break;
     case 3:
         printf("Game Over !\n");
@@ -99,7 +95,7 @@ void ExibeTabuleiro(char tabuleiro[10][10], char mascara[10][10]){
                 std::cout << blue << " " << mascara[i][j] << normal;
                 break;
             
-            case 'P':
+            case 'B':
                 std::cout << green << " " << mascara[i][j] << normal;
                 break;
             
@@ -123,38 +119,87 @@ void ExibeTabuleiro(char tabuleiro[10][10], char mascara[10][10]){
 }
 
 void PosicionaBarcos(char tabuleiro[10][10]){
-    // Coloca X barcos no tabuleiro, no caso 10
-    int quantidade = 10, quantidade_posicionada=0;
+    // Coloca 10 barcos no tabuleiro
+    int quantidade = 5, quantidade_posicionada=0;
 
     while(quantidade_posicionada < quantidade){
-        int linha_aleatoria = rand() % 10;
-        int coluna_aleatoria = rand() % 10;
+        LimpaTela();
+        printf("Insira %d barcos no tabuleiro do Player1\n", quantidade-quantidade_posicionada);
+        int linha_posicionada;
+        int coluna_posicionada;
 
-        if (tabuleiro[linha_aleatoria][coluna_aleatoria] == 'A'){
-            // Posiciona os 10 barcos aleatorios
-            tabuleiro[linha_aleatoria][coluna_aleatoria] = 'P';
+        printf("Insira a linha a ser Poscionada o Barco: \n");
+        scanf("%d", &linha_posicionada);
+        printf("Insira a coluna a ser Poscionada o Barco: \n");
+        scanf("%d", &coluna_posicionada);
 
-            // Aumenta a quantidade para evitar barcos na mesma posicao
-            quantidade_posicionada++;
-        }
+        tabuleiro[linha_posicionada][coluna_posicionada] = 'B';
+
+        quantidade_posicionada++;
     }
+}
 
+void PosicionaBarcosPlayer2(char tabuleiro[10][10]){
+    // Coloca 10 barcos no tabuleiro
+    int quantidade = 5, quantidade_posicionada=0;
+
+    while(quantidade_posicionada < quantidade){
+        LimpaTela();
+        printf("Insira %d barcos no tabuleiro do Player2\n", quantidade-quantidade_posicionada);
+        int linha_posicionada;
+        int coluna_posicionada;
+
+        printf("Insira a linha a ser Poscionada o Barco: \n");
+        scanf("%d", &linha_posicionada);
+        printf("Insira a coluna a ser Poscionada o Barco: \n");
+        scanf("%d", &coluna_posicionada);
+
+        tabuleiro[linha_posicionada][coluna_posicionada] = 'B';
+
+        quantidade_posicionada++;
+    }
 }
 
 void VerificaTiro(char tabuleiro[10][10], int linha_jogada, int coluna_jogada, int *pontuacao, std::string *mensagem){
-    switch (tabuleiro[linha_jogada][coluna_jogada]){
-    case 'P':
+    // 00 01 02
+    // 10 11 12
+    // 20 21 22
+    if (tabuleiro[linha_jogada][coluna_jogada] == 'B'){
         *pontuacao += 10;
-        *mensagem = "Afundo um barco pequeno (10pts)";
-        break;
-    
-    case 'A':
-        *mensagem = "Nao afundou nenhum barco";
-        break;
+        *mensagem = "Afundo ! (10pts)";
+    }
+    // Jogou e deu Agua, Verifica se ao redor tem B
+    else if (tabuleiro[linha_jogada][coluna_jogada] == 'A'){
+        int verificador = 0;
+        if (tabuleiro[linha_jogada+1][coluna_jogada] == 'B'){
+            verificador++;
+        }
+        if (tabuleiro[linha_jogada-1][coluna_jogada] == 'B'){
+            verificador++;
+        }
+        if (tabuleiro[linha_jogada][coluna_jogada+1] == 'B'){
+            verificador++;
+        }
+        if (tabuleiro[linha_jogada][coluna_jogada-1] == 'B'){
+            verificador++;
+        }
+        if (tabuleiro[linha_jogada+1][coluna_jogada+1] == 'B'){
+            verificador++;
+        }
+        if (tabuleiro[linha_jogada-1][coluna_jogada-1] == 'B'){
+            verificador++;
+        }
+        // Mostra mensagem
+        if (verificador > 0){
+            *mensagem = "Passou perto";
+        }
+        else{
+            *mensagem = "Errrroooouuuu";
+        }
     }
 }
 
-void JogoAlone(std::string nome_do_jogador){
+void Jogo(std::string nome_do_jogador, std::string nome_do_jogador2){
     char tabuleiro[10][10];
     char mascara[10][10];
     int i, j;
@@ -183,6 +228,45 @@ void JogoAlone(std::string nome_do_jogador){
         linhaJogada = -1; // So para entrar no while
         colunaJogada = -1; // So para entrar no while
         while( (linhaJogada < 0 || linhaJogada > 9) || (colunaJogada < 0 || colunaJogada > 9) ){
+            printf("%s, Digite uma linha: \n", nome_do_jogador2.c_str());
+            scanf("%d", &linhaJogada);
+            printf("%s, Digite uma coluna: \n", nome_do_jogador2.c_str());
+            scanf("%d", &colunaJogada);
+        }
+
+        VerificaTiro(tabuleiro, linhaJogada, colunaJogada, &pontuacao, &mensagem); // & = Ponteiro para haver a troca de valores da variavel
+
+        // Revela a posicao jogada
+        mascara[linhaJogada][colunaJogada] = tabuleiro[linhaJogada][colunaJogada];
+
+        tentativas++;
+    }
+
+    int prossiga = 0;
+    printf("Type 1 to continue the Game\n");
+    scanf("%d", &prossiga);
+    if (prossiga != 1){
+        exit(1); // Seria o Break
+    }
+
+    // ------ Vez do Player 1 JOGAR ----------
+    IniciaTabuleiro(tabuleiro, mascara);
+    PosicionaBarcosPlayer2(tabuleiro);
+    tentativas = 0;
+    pontuacao = 0;
+    while (tentativas < maximo_de_tentativa){
+        LimpaTela();
+        ExibeMapa();
+        ExibeTabuleiro(tabuleiro, mascara);
+
+        printf("Pontos: %d\n", pontuacao);
+        printf("Tentativas Restantes: %d\n", maximo_de_tentativa-tentativas);
+        printf("%s\n", mensagem.c_str());
+
+        // Verificando Entradas
+        linhaJogada = -1; // So para entrar no while
+        colunaJogada = -1; // So para entrar no while
+        while( (linhaJogada < 0 || linhaJogada > 9) || (colunaJogada < 0 || colunaJogada > 9) ){
             printf("%s, Digite uma linha: \n", nome_do_jogador.c_str());
             scanf("%d", &linhaJogada);
             printf("%s, Digite uma coluna: \n", nome_do_jogador.c_str());
@@ -195,197 +279,6 @@ void JogoAlone(std::string nome_do_jogador){
         mascara[linhaJogada][colunaJogada] = tabuleiro[linhaJogada][colunaJogada];
 
         tentativas++;
-        // if tentativas == maximo...:
-            // break
-    }
-
-    printf("Game Over :\n");
-    printf("1 - Play Again\n");
-    printf("2 - Go to Menu\n");
-    printf("3 - Exit\n");
-    int opcao; // Opcao para continuar
-    scanf("%d", &opcao);
-    switch (opcao){
-    case 1:
-        JogoAlone(nome_do_jogador);
-        break;
-    case 2:
-        MenuInicial();
-        break;
-    }
-}
-
-// ----------------- JOGO COM DOIS JOGADORES --------------------- //
-
-void VerificaTiro_DoPlayer2(int linha_tabuleiro, int coluna_tabuleiro, int linha_jogada, int coluna_jogada, int *pontuacao){ //std::string *mensagem
-    char tabuleiro[linha_tabuleiro][coluna_tabuleiro];
-
-    switch (tabuleiro[linha_jogada][coluna_jogada]){
-    case 'P':
-        *pontuacao += 10;
-        // *mensagem = "Afundo um barco pequeno (10pts)";
-        break;
-    
-    case 'A':
-        // *mensagem = "Nao afundou nenhum barco";
-        break;
-    }
-}
-
-void ExibeTabuleiroPlayer1(int linha_tabuleiro, int coluna_tabuleiro){
-    char tabuleiro[linha_tabuleiro][coluna_tabuleiro];
-    char mascara[linha_tabuleiro][coluna_tabuleiro];
-    
-    int i, j;
-    // INICIA TABULEIRO PLAYER 1
-    for (i = 0; i < linha_tabuleiro; i++){
-        for (j = 0; j < coluna_tabuleiro; j++){
-            tabuleiro[i][j] = 'A';
-            mascara[i][j] = '*';
-        }
-    }
-    // Printa Mascara
-    for (i = 0; i < linha_tabuleiro; i++){
-        printf("%d -",i); // Mapea as linhas
-        for (j = 0; j < coluna_tabuleiro; j++){
-            printf("%c", mascara[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-    // Printa Tabuleiro
-        // Comente esse codigo para n aparecer as repostas
-    for (i = 0; i < linha_tabuleiro; i++){
-        for (j = 0; j < coluna_tabuleiro; j++){
-            printf("%c ", tabuleiro[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-// MAPEA AS COLUNAS
-void ExibeMapaJogoTogether(int coluna_tabuleiro){
-    // Indicando colunas (Mapea)
-    for (int cont = 0; cont < coluna_tabuleiro; cont++){
-        if (cont == 0){
-            printf("    ");
-        }
-        printf("%d ", cont);
-    }
-    printf("\n");
-    for (int cont = 0; cont < coluna_tabuleiro; cont++){
-        if (cont == 0){
-            printf("    ");
-        }
-        printf("| ");
-    }
-    printf("\n");
-}
-
-void PosicionaBarcosPlayer1(int linha_tabuleiro, int coluna_tabuleiro){
-    char tabuleiro[linha_tabuleiro][coluna_tabuleiro];
-    printf("Insira 5 barcos no tabuleiro do Player1\n");
-
-    // Coloca 5 barcos no tabuleiro
-    int quantidade = 5, quantidade_posicionada=0;
-
-    while(quantidade_posicionada < quantidade){
-        int linha_posicionada;
-        int coluna_posicionada;
-        printf("Insira a linha a ser Poscionada o Barco: \n");
-        scanf("%d", &linha_posicionada);
-        printf("Insira a coluna a ser Poscionada o Barco: \n");
-        scanf("%d", &coluna_posicionada);
-
-        tabuleiro[linha_posicionada][coluna_posicionada] = 'B';
-
-        quantidade_posicionada++;
-    }
-
-}
-
-void IniciaTabuleiroPlayer2(int linha_tabuleiro, int coluna_tabuleiro){ // nt tabuleiro[][], char mascara[][], i
-    char tabuleiro[linha_tabuleiro][coluna_tabuleiro];
-    char mascara[linha_tabuleiro][coluna_tabuleiro];
-    // i = linha, j = coluna
-    int i, j;
-    // Preenchendo o tabuleiro
-    for (i = 0; i < linha_tabuleiro; i++){
-        for (j = 0; j < coluna_tabuleiro; j++){
-            tabuleiro[i][j] = 'A';
-            mascara[i][j] = '*';
-        }
-    }
-}
-
-void PosicionaBarcosPlayer2(int linha_tabuleiro, int coluna_tabuleiro){
-    char tabuleiro[linha_tabuleiro][coluna_tabuleiro];
-    printf("Insira 5 barcos no tabuleiro do Player2\n");
-
-    // Coloca 5 barcos no tabuleiro
-    int quantidade = 5, quantidade_posicionada=0;
-
-    while(quantidade_posicionada < quantidade){
-        int linha_posicionada;
-        int coluna_posicionada;
-        printf("Insira a linha a ser Poscionada o Barco: \n");
-        scanf("%d", &linha_posicionada);
-        printf("Insira a coluna a ser Poscionada o Barco: \n");
-        scanf("%d", &coluna_posicionada);
-
-        tabuleiro[linha_posicionada][coluna_posicionada] = 'B';
-
-        quantidade_posicionada++;
-    }
-
-}
-
-void JogoTogether(std::string player1, std::string player2){
-    int linha_tabuleiro, coluna_tabuleiro;
-    printf("Insira a Ordem do Tabuleiro. Ex: 4x4 LinhaxColuna\n");
-    scanf("%d %d", &linha_tabuleiro, &coluna_tabuleiro);
-    char tabuleiro[linha_tabuleiro][coluna_tabuleiro];
-    char mascara[linha_tabuleiro][coluna_tabuleiro];
-
-    int i, j;
-    int linhaJogada, colunaJogada;
-    int status_jogo = 1;
-    int pontuacao = 0;
-    int tentativas = 1, maximo_de_tentativa = 5;
-
-    // IniciaTabuleiroPlayer1(linha_tabuleiro, coluna_tabuleiro); -> Passei para funcao ExibeTabuleiroPLayer1
-    PosicionaBarcosPlayer1(linha_tabuleiro, coluna_tabuleiro);
-    
-    // IniciaTabuleiroPlayer2(linha_tabuleiro, coluna_tabuleiro);
-    // PosicionaBarcosPlayer2(linha_tabuleiro, coluna_tabuleiro);
-
-    while (tentativas == 1){
-        LimpaTela();
-        ExibeMapaJogoTogether(coluna_tabuleiro);
-        ExibeTabuleiroPlayer1(linha_tabuleiro, coluna_tabuleiro);
-
-    //     printf("Pontos: %d\n", pontuacao);
-    //     printf("Tentativas Restantes: %d\n", maximo_de_tentativa-tentativas);
-    //     printf("%s\n", mensagem.c_str());
-
-        // Verificando Entradas
-        // linhaJogada = -1; // So para entrar no while
-        // colunaJogada = -1; // So para entrar no while
-        // while( (linhaJogada < 0 || linhaJogada > 9) || (colunaJogada < 0 || colunaJogada > 9) ){
-            printf("%s, Digite uma linha: \n", player2.c_str());
-            scanf("%d", &linhaJogada);
-            printf("%s, Digite uma coluna: \n", player2.c_str());
-            scanf("%d", &colunaJogada);
-        // }
-
-        VerificaTiro_DoPlayer2(linha_tabuleiro, coluna_tabuleiro, linhaJogada, colunaJogada, &pontuacao); // &mensagem - & = Ponteiro para haver a troca de valores da variavel
-
-        // Revela a posicao jogada
-        mascara[linhaJogada][colunaJogada] = tabuleiro[linhaJogada][colunaJogada];
-
-        // tentativas++;
-    //     // if tentativas == maximo...:
-    //         // break
     }
 
     // printf("Game Over :\n");
@@ -396,13 +289,12 @@ void JogoTogether(std::string player1, std::string player2){
     // scanf("%d", &opcao);
     // switch (opcao){
     // case 1:
-    //     JogoAlone(nome_do_jogador);
+    //     Jogo(nome_do_jogador, nome_do_jogador2);
     //     break;
     // case 2:
     //     MenuInicial();
     //     break;
     // }
-
 }
 
 int main(){
@@ -413,4 +305,3 @@ int main(){
 
     return 0;
 }
-
